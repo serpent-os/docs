ad9776c44a33ee3149f79e23e27d48995e99cbbf
Ikey Doherty <ikey@serpentos.com>
2023-05-13 13:14:19 +0100

    Initial docusaurus install
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/ikey/serpent/docs/node_modules/infima/dist/css/default/default.css'),
+  require('/home/ikey/serpent/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/ikey/serpent/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/ikey/serpent/docs/src/css/custom.css'),
+];
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# docs
-Serpent OS documentation
+# Website
+
+This website is built using [Docusaurus 2](https://docusaurus.io/), a modern static website generator.
+
+### Installation
+
+```
+$ yarn
+```
+
+### Local Development
+
+```
+$ yarn start
+```
+
+## docs
+
+In progress documentation for Serpent OS.
+
+Using Docusaurus.
